9a00e8c766f4993c3be32aa608b401e5696b8233
iqbal <muhammad.mahendra@students.amikom.ac.id>
2021-05-29 02:15:50 +0700

    Initial commit

---
+++ bin/Operator_Assignment.dart
@@ -0,0 +1,35 @@
+void main(){
+
+  /*
+    Operator Assignment adalah operator yg menggunakan
+    tanda sama dengan ( = ) untuk mengisi sebuah nilai dalam
+    suatu variabel. Operator assignment digunakan untuk menyatakan
+    suatu perintah atau fungsi perhitungan untuk memberikan nilai pada
+    variabel dalam bentuk yang disingkat
+   */
+
+  int a = 10;
+  // Assignment operator penjumlahan
+  a += 10;
+  print('Nilai dari a = $a');
+
+  int b = 20;
+  // Assignment operator pengurangan
+  b -= 10;
+  print('Nilai dari b = $b');
+
+  double c = 20;
+  // Assignment operator perkalian
+  c *= 10;
+  print('Nilai dari c = $c');
+
+  double d = 100;
+  // Assignment operator pembagian
+  d /= 25;
+  print('Nilai dari d = $d');
+
+  int e = 10;
+  // Assignment operator modulus
+  e %= 7;
+  print('Nilai dari e = $e');
+}
